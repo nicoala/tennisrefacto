ea49756fa959b8c7a5b395e1898fc6fcb675a798
nicolaschazeaux <n.chazeaux@gmail.com>
2017-06-28 12:18:16 +0200

    Refacto Tennis Game 1 - readability

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,28 +1,25 @@
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
-
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName == "player1") {
+            playerOneScore++;
+        }
+        else {
+            playerTwoScore++;
+        }
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+
+
+        if (playerOneScore == playerTwoScore)
         {
-            switch (m_score1)
+            switch (playerOneScore)
             {
                 case 0:
                     score = "Love-All";
@@ -38,23 +35,23 @@ public class TennisGame1 implements TennisGame {
                     break;
 
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
+        } else if (playerOneScore >=4 || playerTwoScore >=4) {
+            int minusResult = playerOneScore - playerTwoScore;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
             else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
+        } else {
+            for (int i=1; i<3; i++) {
+
+                if (i==1) {
+                    tempScore = playerOneScore;
+                } else {
+                    score += "-";
+                    tempScore = playerTwoScore;
+                }
+
+                switch(tempScore) {
                     case 0:
                         score+="Love";
                         break;
@@ -70,6 +67,7 @@ public class TennisGame1 implements TennisGame {
                 }
             }
         }
+
         return score;
     }
 }
