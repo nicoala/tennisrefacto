25bf5b95b5d4c5dee0cd457cbc23e3a3125d49c6
nicolaschazeaux <n.chazeaux@gmail.com>
2017-06-28 17:02:05 +0200

    Refacto completed without touching Player class

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -6,48 +6,46 @@ public class TennisGame2 implements TennisGame
     Player playerTwo;
 
     public TennisGame2(String player1Name, String player2Name) {
-
         playerOne = new Player(player1Name, 0);
         playerTwo = new Player(player2Name, 0);
     }
 
+    public void wonPoint(String player) {
+        if (playerOne.isNamed(player))
+            playerOne.scores();
+        else
+            playerTwo.scores();
+    }
+
     public String getScore(){
 
-        if (playerOne.hasSameScore(playerTwo) && !playerOne.isCloseToWin())
-        {
-            score = playerOne.tennisScore();
-            score += "-All";
+        if(playerOne.hasSameScore(playerTwo)) {
+            score = closeGameIsTied();
+        } else {
+            score = notADraw();
         }
-
-        if (playerOne.hasSameScore(playerTwo) && playerOne.isCloseToEndOfGame()) {
-
-            score = TennisScore.DEUCE.toString();
-        }
-
-        if(!playerOne.hasSameScore(playerTwo)) {
-
-            if ( playerOne.isCloseToWin() || playerTwo.isCloseToWin()) {
-                score = playerOne.gameOnTheLineVs(playerTwo);
-            } else {
-                score = playerOne.tennisScore() + "-" + playerTwo.tennisScore();
-            }
-        }
-
         return score;
     }
 
-    public void P1Score(){
-        playerOne.scores();
+    private String closeGameIsTied() {
+        if (playerOne.isCloseToEndOfGame()) {
+            return TennisScore.DEUCE.toString();
+        } else {
+            return gameIsTied();
+        }
     }
 
-    public void P2Score(){
-        playerTwo.scores();
+    private String gameIsTied() {
+        String gameScore = playerOne.tennisScore();
+        gameScore += "-All";
+        return gameScore;
     }
 
-    public void wonPoint(String player) {
-        if (playerOne.isNamed(player))
-            P1Score();
-        else
-            P2Score();
+    private String notADraw() {
+        if (playerOne.isCloseToWin() || playerTwo.isCloseToWin()) {
+            return playerOne.gameOnTheLineVs(playerTwo);
+        } else {
+            return playerOne.tennisScore() + "-" + playerTwo.tennisScore();
+        }
     }
 }
